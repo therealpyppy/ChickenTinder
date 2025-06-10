8d51ef9b466f5ab637d8f20e8050ba879790c39b
therealpeppy <furghadmanar@gmail.com>
2025-06-10 14:32:09 -0500

    add firebase firestore and emulating

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { FirestoreExample } from "./firestore";
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+      <FirestoreExample />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
